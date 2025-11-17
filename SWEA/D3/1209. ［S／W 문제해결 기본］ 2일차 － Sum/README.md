99b508ec7f8078e53459038f8d8872ff4529baff
yuy <57798944+youngungyun@users.noreply.github.com>
2025-11-18 00:02:32 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 357 ms, Memory: 102,704 KB -BaekjoonHub

---
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 2일차 - Sum - 1209 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV13_BWKACUCFAYh) 
+
+### 성능 요약
+
+메모리: 102,704 KB, 시간: 357 ms, 코드길이: 3,697 Bytes
+
+### 제출 일자
+
+2025-11-18 00:02
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;
+// double b = 1.0;
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+        Scanner sc = new Scanner(System.in);
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= 10; test_case++)
+        {
+            int t = sc.nextInt();
+            int[][] matrix = new int[100][100];
+
+            for (int row = 0; row < 100; row++) {
+                for (int col = 0; col < 100; col++) {
+                    matrix[row][col] = sc.nextInt();
+                }
+            }
+
+            int max = 0;
+
+            // 행, 열 최댓값 구하기
+            for (int x = 0; x < 100; x++) {
+                int rowSum = 0;
+                int colSum = 0;
+                for (int y = 0; y < 100; y++) {
+                    rowSum += matrix[x][y];
+                    colSum += matrix[y][x];
+                }
+                max = Math.max(max, Math.max(rowSum, colSum));
+            }
+
+            // 대각선 최댓값 구하기
+            int leftDiagonalSum = 0;
+            int rightDiagonalSum = 0;
+            for (int i = 0; i < 100; i++) {
+                leftDiagonalSum += matrix[i][i];
+                rightDiagonalSum += matrix[i][99 - i];
+            }
+
+            max = Math.max(max, Math.max(leftDiagonalSum, rightDiagonalSum));
+
+            System.out.printf("#%d %d\n", test_case, max);
+        }
+    }
+}
