e2b78cbf6a0570308d52174a2fad3728e03c7c2c
yuy <57798944+youngungyun@users.noreply.github.com>
2025-12-03 17:30:57 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String word = br.readLine();
+
+        System.out.println(word.contains("gori") ? "YES" : "NO");
+    }
+}
