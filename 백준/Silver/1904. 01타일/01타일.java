e56f47808a88c241f91b31d3816b56b0975cabe6
yuy <57798944+youngungyun@users.noreply.github.com>
2025-12-05 21:38:50 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18612 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // dp[n] = dp[n-1] + dp[n-2]
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
