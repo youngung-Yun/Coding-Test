639c575af62a0a144b509e26c2c8a9b356092294
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-06 01:22:21 +0900

    Remove unnecessary variable and clean up code

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -3,9 +3,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    private static int result = 50 * 50 * 23 + 1;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -23,3 +20,4 @@ public class Main {
         System.out.println(count);
     }
 }
+
