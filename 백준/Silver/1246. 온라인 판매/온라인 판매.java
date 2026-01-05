9b3cf655ac41dd3e27d59d4618470f79c9a38199
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-06 01:02:18 +0900

    [Silver IV] Title: 온라인 판매, Time: 128 ms, Memory: 16092 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int result = 50 * 50 * 23 + 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] maxPrices = new int[m];
+        for (int i = 0; i < m; i++) {
+            maxPrices[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(maxPrices);
+        int price = 1_000_001;
+        int income = 0;
+        for (int i = 0; i < m; i++) {
+            // maxPrices[i] 가격으로 총 min(m-i, n)명이 구입
+            int total = maxPrices[i] * Integer.min(n, m - i);
+            if (income < total) {
+                price = maxPrices[i];
+                income = total;
+            }
+        }
+        System.out.println(price + " " + income);
+    }
+}
