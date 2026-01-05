e470979e2a834a7fa966abda60fd8e73a051fefc
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-06 01:11:31 +0900

    [Silver V] Title: 국회의원 선거, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int result = 50 * 50 * 23 + 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int myVotes = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> othersVotes = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < n - 1; i++) {
+            othersVotes.add(Integer.parseInt(br.readLine()));
+        }
+        int count = 0;
+        while (!othersVotes.isEmpty() && myVotes <= othersVotes.peek()) {
+            othersVotes.add(othersVotes.poll() - 1);
+            ++myVotes;
+            ++count;
+        }
+        System.out.println(count);
+    }
+}
