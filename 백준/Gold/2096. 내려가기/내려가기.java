ce062fce33d5cc7c13bd6a70fbe1f5b03c897f82
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-07 23:49:41 +0900

    [Gold V] Title: 내려가기, Time: 488 ms, Memory: 56408 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private static int result = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] minDp = new int[n][3];
+        int[][] maxDp = new int[n][3];
+
+        for (int i = 0; i < 3; i++) {
+            minDp[0][i] = board[0][i];
+            maxDp[0][i] = board[0][i];
+        }
+        for (int i = 1; i < n; i++) {
+            minDp[i][0] = Integer.min(minDp[i-1][0], minDp[i-1][1]) + board[i][0];
+            maxDp[i][0] = Integer.max(maxDp[i-1][0], maxDp[i-1][1]) + board[i][0];
+
+            minDp[i][1] = Integer.min(minDp[i-1][0], Integer.min(minDp[i-1][1], minDp[i-1][2])) + board[i][1];
+            maxDp[i][1] = Integer.max(maxDp[i-1][0], Integer.max(maxDp[i-1][1], maxDp[i-1][2])) + board[i][1];
+
+            minDp[i][2] = Integer.min(minDp[i-1][1], minDp[i-1][2]) + board[i][2];
+            maxDp[i][2] = Integer.max(maxDp[i-1][1], maxDp[i-1][2]) + board[i][2];
+        }
+
+        int min = 900_001;
+        int max = 0;
+        for (int i = 0; i < 3; i++) {
+            min = Integer.min(min, minDp[n-1][i]);
+            max = Integer.max(max, maxDp[n-1][i]);
+        }
+
+        System.out.println(max + " " + min);
+    }
+
+}
