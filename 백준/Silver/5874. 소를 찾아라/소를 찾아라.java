feacf53771e9a0e1ac68454cef3e8a83408540f4
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-09 22:58:38 +0900

    [Silver IV] Title: 소를 찾아라, Time: 112 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/5874. 소를 찾아라/소를 찾아라.java
+++ 백준/Silver/5874. 소를 찾아라/소를 찾아라.java
@@ -7,28 +7,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String brackets = br.readLine();
         int l = brackets.length();
-        int[] backLegs = new int[l];
         int[] frontLegs = new int[l];
-        for (int i = 1; i < l; i++) {
-            if (brackets.charAt(i) == '(' && brackets.charAt(i - 1) == '(') {
-                backLegs[i] = backLegs[i-1] + 1;
+        for (int i = l - 2; i >= 0; i--) {
+            if (brackets.charAt(i) == ')' && brackets.charAt(i + 1) == ')') {
+                frontLegs[i] = frontLegs[i+1] + 1;
             } else {
-                backLegs[i] = backLegs[i-1];
-            }
-            if (brackets.charAt(i) == ')' && brackets.charAt(i - 1) == ')') {
-                frontLegs[i] = frontLegs[i-1] + 1;
-            } else {
-                frontLegs[i] = frontLegs[i-1];
+                frontLegs[i] = frontLegs[i+1];
             }
         }
         int result = 0;
-        int current = 0;
         for (int i = 1; i < l; i++) {
-            if (current < backLegs[i]) {
-                result += frontLegs[l-1] - frontLegs[i + 1];
-                current = backLegs[i];
+            if (brackets.charAt(i) == '(' && brackets.charAt(i - 1) == '(') {
+                result += frontLegs[i];
             }
-        }
+            }
         System.out.println(result);
     }
 }
