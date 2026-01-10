cc5e4f2d0e6f767a51e4104e97ab3513d0c40018
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-11 02:59:30 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 232 ms, Memory: 22704 KB -BaekjoonHub

--- 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
@@ -18,21 +18,14 @@ public class Main {
                 ++oddCount;
                 continue;
             }
-            // oddCount가 초과되었을 땐 k까지 줄이기 위해 left 이동해야함
-            // oddCount가 k여도 array[left]가 짝수가 될 때 까지 이동
-            if (left < right && (oddCount > k || array[left] % 2 == 1)) {
+            while (left < right && oddCount > k) {
                 if (array[left] % 2 == 1) {
                     --oddCount;
                 }
                 ++left;
             }
-            while (left < right && array[left] % 2 == 1) {
-                --oddCount;
-                ++left;
-            }
-
             // 부분 수열의 요소 개수는 right - left + 1개
-            // 여기에 현재 홀수 개수인 oddCount를 뺸 것이 짝수의 개수
+            // 여기에 현재 홀수 개수인 oddCount를 뺌
             int length = (right - left + 1) - oddCount;
             answer = Integer.max(answer, length);
         }
