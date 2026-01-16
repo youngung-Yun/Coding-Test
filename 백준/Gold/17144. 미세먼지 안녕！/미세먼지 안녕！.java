fa1c56d1f7a2dd40411abe6f205da2ffe281de2a
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-16 19:00:09 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 376 ms, Memory: 28540 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,17 +1,14 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] dirs = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-    static int[] top;
-    static int[] bottom;
+    // 공기 청정기의 윗부분 위치
+    static int[] airPurifier;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,18 +18,14 @@ public class Main {
         int c = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(st.nextToken());
 
-        boolean hasFoundAirFurifier = false;
 
         int[][] room = new int[r][c];
         for (int row = 0; row < r; row++) {
             st = new StringTokenizer(br.readLine());
             for (int col = 0; col < c; col++) {
                 int num = Integer.parseInt(st.nextToken());
-                if (num == -1 && !hasFoundAirFurifier) {
-                    // 공기청정기 위치 저장
-                    top = new int[] {row, col};
-                    bottom = new int[] {row+1, col};
-                    hasFoundAirFurifier = true;
+                if (num == -1 && room[row-1][col] == -1) {
+                    airPurifier = new int[] {row-1, col};
                 }
                 room[row][col] = num;
             }
@@ -42,7 +35,6 @@ public class Main {
             spreadDust(room, r, c);
             runAirFurifierTop(room, r, c);
             runAirFurifierBottom(room, r, c);
-
         }
 
         int total = 2;
@@ -55,8 +47,8 @@ public class Main {
     }
 
     static void runAirFurifierTop(int[][] room, int r, int c) {
-        int x = top[0];
-        int y = top[1] + 1;
+        int x = airPurifier[0];
+        int y = airPurifier[1] + 1;
         int now = room[x][y];
         room[x][y] = 0;
         // 오른쪽으로
@@ -81,7 +73,7 @@ public class Main {
             --y;
         }
         // 아래로
-        while (x < top[0] - 1) {
+        while (x < airPurifier[0] - 1) {
             int tmp = room[x+1][y];
             room[x+1][y] = now;
             now = tmp;
@@ -90,8 +82,8 @@ public class Main {
     }
 
     static void runAirFurifierBottom(int[][] room, int r, int c) {
-        int x = bottom[0];
-        int y = bottom[1] + 1;
+        int x = airPurifier[0] + 1;
+        int y = airPurifier[1] + 1;
         int now = room[x][y];
         room[x][y] = 0;
         // 오른쪽으로
@@ -116,7 +108,7 @@ public class Main {
             --y;
         }
         // 위로
-        while (x > bottom[0] + 1) {
+        while (x > airPurifier[0] + 1 + 1) {
             int tmp = room[x-1][y];
             room[x-1][y] = now;
             now = tmp;
@@ -134,34 +126,28 @@ public class Main {
          * 	   - 미세먼지는 한 번에 퍼지기 때문에 더해진/빼진 값만 저장할 2차원 배열 필요
          * 5. 현재 방 상태에 퍼진 후 변한 미세먼지 값 더함
          */
-        List<int[]> dustList = new ArrayList<>();
-        for (int row = 0; row < r; ++row) {
-            for (int col = 0; col < c; ++col) {
-                if (room[row][col] > 0) {
-                    dustList.add(new int[] {row, col});
-                }
-            }
-        }
         int[][] dustDiff = new int[r][c];
 
-        for (int[] dust : dustList) {
-            int x = dust[0];
-            int y = dust[1];
-            for (int[] dir : dirs) {
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-                if (nx < 0 || ny < 0 || nx >= r || ny >= c) {
+        for (int row = 0; row < r; ++row) {
+            for (int col = 0; col < c; ++col) {
+                if (room[row][col] <= 0) {
                     continue;
                 }
-                if (room[nx][ny] == -1) {
-                    continue;
+                for (int[] dir : dirs) {
+                    int nx = row + dir[0];
+                    int ny = col + dir[1];
+                    if (nx < 0 || ny < 0 || nx >= r || ny >= c) {
+                        continue;
+                    }
+                    if (room[nx][ny] == -1) {
+                        continue;
+                    }
+                    int spreadDust = room[row][col] / 5;
+                    dustDiff[nx][ny] += spreadDust;
+                    dustDiff[row][col] -= spreadDust;
                 }
-                int spreadDust = room[x][y] / 5;
-                dustDiff[nx][ny] += spreadDust;
-                dustDiff[x][y] -= spreadDust;
             }
         }
-
         // 퍼진 미세먼지 한 번에 계산
         for (int row = 0; row < r; row++) {
             for (int col = 0; col < c; col++) {
