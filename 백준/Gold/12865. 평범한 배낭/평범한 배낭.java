073af11f6eaa536adf2ae78304cb273c95660801
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-16 21:32:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 53800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,40 +1,46 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
-
-        // [0]: 무게, [1]: 가치
-        int[][] items = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            items[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(st.nextToken());
+        int capacity = Integer.parseInt(st.nextToken());
+        Item[] items = new Item[n+1];
+        items[0] = new Item(0, 0);
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            items[i] = new Item(weight, value);
         }
 
-        // dp[i][w] = 최대 용량이 w인 배낭에 i번째 물건까지 넣었을 때의 최대 가치
-        // w + weight[i] > limit면 물건 못 넣음. dp[i][w] = dp[i-1][w]
-        // w + weight[i] <= limit면 dp[i][w] = // 물건 넣을 수 있음
-        int[][] dp = new int[n + 1][k + 1];
+        int[][] dp = new int[n+1][capacity+1];
         for (int i = 1; i <= n; i++) {
-            int weight = items[i - 1][0];
-            int value = items[i - 1][1];
-
-            for (int w = 1; w <= k; w++) {
-                if (w < weight) { // 물건 못 넣음
-                    dp[i][w] = dp[i - 1][w];
-                }
-                else {
-                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weight] + value);
+            Item item = items[i];
+            for (int w = 1; w <= capacity; w++) {
+                if (w < item.weight) {
+                    dp[i][w] = dp[i-1][w];
+                } else {
+                    dp[i][w] = Integer.max(dp[i-1][w], dp[i-1][w - item.weight] + item.value);
                 }
             }
-        }     
-        
-        System.out.println(dp[n][k]);
+        }
+        System.out.println(dp[n][capacity]);
+    }
+
+
+    static class Item {
+        public int weight;
+        public int value;
+
+        public Item(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
     }
 }
