a19cc2b4933dcb2ba83d43226d438eda19464ab0
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-16 21:46:18 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18672 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,45 +1,25 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        // dp[i][k] = first[0:i]와 second[0:k]의 lcs 길이
-        // i == 0 || k == 0이면 dp[i][k] = 0
-        // first[i] == second[k]면: dp[i][k] = dp[i-1][k-1] + 1
-        // first[i] != second[k]면: dp[i][k] = max(dp[i-1][k], dp[i][k-1])
+        String a = br.readLine();
+        String b = br.readLine();
 
-        String firstString = br.readLine();
-        String secondString = br.readLine();
+        int[][] dp = new int[a.length()+1][b.length()+1];
 
-        int[][] dp = new int[firstString.length()][secondString.length()];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int i = 1; i <= a.length(); i++) {
+            for (int k = 1; k <= b.length(); k++) {
+                if (a.charAt(i-1) == b.charAt(k-1)) {
+                    dp[i][k] = dp[i-1][k-1] + 1;
+                } else {
+                    dp[i][k] = Integer.max(dp[i-1][k], dp[i][k-1]);
+                }
+            }
         }
-        int answer = lcs(dp, firstString, secondString, firstString.length() - 1, secondString.length() - 1);
-
-        System.out.println(answer);
-    }
-
-    private static int lcs(int[][] dp, String first, String second, int firstIndex, int secondIndex) {
-        if (firstIndex < 0 || secondIndex < 0) {
-            return 0;
-        }
-
-        if (dp[firstIndex][secondIndex] != -1) {
-            return dp[firstIndex][secondIndex];
-        }
-
-        if (first.charAt(firstIndex) == second.charAt(secondIndex)) {
-            dp[firstIndex][secondIndex] = lcs(dp, first, second, firstIndex - 1, secondIndex - 1) + 1;
-        }
-        else {
-            dp[firstIndex][secondIndex] = Math.max(lcs(dp, first, second, firstIndex - 1, secondIndex), 
-            lcs(dp, first, second, firstIndex, secondIndex - 1));
-        }
-        return dp[firstIndex][secondIndex];
+        System.out.println(dp[a.length()][b.length()]);
     }
 }
