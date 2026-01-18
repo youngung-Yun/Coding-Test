5350459523c39a7a1a6d73146a768bef880a4a3e
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-18 14:31:12 +0900

    [D3] Title: 최대 성적표 만들기, Time: 175 ms, Memory: 33,792 KB -BaekjoonHub

---
+++ SWEA/D3/4466. 최대 성적표 만들기/README.md
@@ -0,0 +1,15 @@
+# [D3] 최대 성적표 만들기 - 4466 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWOUfCJ6qVMDFAWg) 
+
+### 성능 요약
+
+메모리: 33,792 KB, 시간: 175 ms, 코드길이: 917 Bytes
+
+### 제출 일자
+
+2026-01-18 14:31
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= t; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int total = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().skip(n - k).sum();
+
+            sb.append('#').append(testCase).append(' ').append(total).append('\n');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb);
+    }
+}
