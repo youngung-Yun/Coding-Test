76336d5c8dcc2b6962486ca720155141c4989b6b
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-18 22:40:19 +0900

    [D3] Title: 구독자 전쟁, Time: 83 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/10200. 구독자 전쟁/README.md
@@ -0,0 +1,15 @@
+# [D3] 구독자 전쟁 - 10200 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXMCXV_qVgkDFAWv) 
+
+### 성능 요약
+
+메모리: 25,472 KB, 시간: 83 ms, 코드길이: 1,037 Bytes
+
+### 제출 일자
+
+2026-01-18 22:40
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/10200. 구독자 전쟁/구독자 전쟁.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= t; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int min = 0;
+            if (a + b < n) {
+                min = 0;
+            } else {
+                min = (a + b) - n;
+            }
+            int max = Integer.min(a, b);
+            sb.append('#').append(testCase).append(' ').append(max).append(' ').append(min).append('\n');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb);
+    }
+}
