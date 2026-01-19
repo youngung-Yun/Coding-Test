6877b52b4f06d99541ac21ab712aacdba0303137
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-19 19:09:28 +0900

    [D3] Title: [Professional] 건초더미, Time: 113 ms, Memory: 31,488 KB -BaekjoonHub

--- SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.java
+++ SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.java
@@ -6,10 +6,6 @@ class Solution
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int[] months = new int[] {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int[] days = new int[] {4, 5, 6, 0, 1, 2, 3};
-
         int t = Integer.parseInt(br.readLine());
         for (int testCase = 1; testCase <= t; testCase++) {
             int n = Integer.parseInt(br.readLine());
