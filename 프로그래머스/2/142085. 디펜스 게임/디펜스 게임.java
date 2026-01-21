963b0d9375be86663c733311083c8c6300ddc100
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-21 14:02:39 +0900

    Refactor defense game logic with simpler priority queue

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -6,36 +6,24 @@ class Solution {
        지금까지 클리어한 라운드 중 사람 가장 많은 라운드 스킵하는게 이득
     */
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Round> pq = new PriorityQueue<>((r1, r2) -> Integer.compare(r2.enemy, r1.enemy));
-        int idx = 0;
-        while (idx < enemy.length) {
-            Round curr = new Round(idx, enemy[idx]);
-            pq.add(curr);
-            n -= enemy[idx];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int round = 0;
+        while (round < enemy.length) {
+            pq.add(enemy[round]);
+            n -= enemy[round];
             if (n < 0) {
                 // 병사 모라잔데 스킵 못하면 거기까지
                 if (k <= 0) {
-                    return idx;
+                    return round;
                 } else {
                     // 스킵 있으면 스킵
-                    Round skip = pq.poll();
-                    n += skip.enemy;
+                    int e = pq.poll();
+                    n += e;
                     k -= 1;
                 }
             }
-            ++idx;
+            ++round;
         }
-        return idx;
-    }
-
-
-    static class Round {
-        public int index;
-        public int enemy;
-
-        public Round(int index, int enemy) {
-            this.index = index;
-            this.enemy = enemy;
-        }
+        return round;
     }
 }
