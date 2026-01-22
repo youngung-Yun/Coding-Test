254f77e71634dcf904144add6ac3ecd65d6b9ad5
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-22 20:18:50 +0900

    Refactor solution method for finding largest square

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -3,23 +3,13 @@ class Solution {
         int row = board.length;
         int col = board[0].length;
         
-        int start = 1;
-        while (start < row && start < col) {
-            // 가로 채우기
-            for (int i = start; i < col; i++) {
-                if (board[start][i] == 1) {
-                    board[start][i] = Integer.min(board[start-1][i-1],
-                    Integer.min(board[start-1][i], board[start][i-1])) + 1;
+        for (int i = 1; i < row; i++) {
+            for (int k = 1; k < col; k++) {
+                if (board[i][k] == 1) {
+                    board[i][k] = Integer.min(board[i-1][k-1],
+                    Integer.min(board[i-1][k], board[i][k-1])) + 1;
                 }
             }
-            // 세로 채우기
-            for (int i = start; i < row; i++) {
-                if (board[i][start] == 1) {
-                    board[i][start] = Integer.min(board[i-1][start-1],
-                   Integer.min(board[i-1][start], board[i][start-1])) + 1;
-                }
-            }
-            start += 1;
         }
         int answer = 0;
         for (int i = 0; i < row; i++) {
