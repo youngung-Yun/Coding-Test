bd9e6e7cef656a7cfbae3a93a2d9b26463d65cf4
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-23 16:55:33 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 55.44 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -4,12 +4,15 @@ class Solution {
     public long solution(int r1, int r2) {
         long answer = 0L;
         for (int x = 0; x <= r2; x++) {
+            // r1의 안 쪽에 있는 점 개수
             long y1 = ((long) Math.sqrt(Math.pow(r1, 2) - Math.pow(x, 2))) + 1L;
             if (x > r1) {
                 y1 = 0L;
+            // r1의 경계에 있는 점은 제외
             } else if (Math.sqrt(Math.pow(r1, 2) - Math.pow(x, 2)) % 1.0 == 0.0) {
                 y1 -= 1;
-            }           
+            }
+            // r2의 안 쪽에 있는 점 개수
             long y2 = ((long) Math.sqrt(Math.pow(r2, 2) - Math.pow(x, 2))) + 1L;
             long dotCount = y2 - y1;
             
