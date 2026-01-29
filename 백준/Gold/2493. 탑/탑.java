0a58fc06136d943293be912774971b37c7564df8
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-29 13:57:35 +0900

    [Gold V] Title: 탑, Time: 592 ms, Memory: 98200 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,34 +1,46 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        int[] recievers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] answer = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
 
-        // [index,height]
-        Deque<int[]> stack = new ArrayDeque<>();
-        for (int i = n - 1; i >= 0; i--) {
-            int[] curr = {i, recievers[i]};
-            while (!stack.isEmpty() && stack.peek()[1] <= curr[1]) {
-                int[] lower = stack.pop();
-                answer[lower[0]] = curr[0] + 1;
-            }
-            stack.push(curr);
-        }
-        while (!stack.isEmpty()) {
-            int[] higher = stack.pop();
-            answer[higher[0]] = 0;
-        }
+		st = new StringTokenizer(br.readLine());
+		Stack<Integer> sin = new Stack<>();
+		Stack<int[]> sout = new Stack<>();
 
-        for (int e : answer) {
-            sb.append(e).append(' ');
-        }
-        System.out.println(sb);
-    }
+		for (int i = 0; i < n; i++) {
+			sin.push(Integer.parseInt(st.nextToken()));
+		}
+
+		int[] result = new int[n];
+		int index = n - 1;
+		for (int i = 0; i < n; i++) {
+			int temp = sin.pop();
+			while (true) {
+				if (sout.isEmpty() || sout.peek()[1] > temp) {
+					sout.push(new int[] { index, temp });
+					index--;
+					break;
+				}
+				result[sout.pop()[0]] = n - i;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append(result[i]).append(" ");
+		}
+
+		System.out.println(sb);
+
+	}
+
 }
