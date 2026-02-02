6ba5d38e4333cb0b5eabb5e6af67aa157ff92718
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-02 14:31:30 +0900

    [Gold V] Title: 하노이 탑, Time: 448 ms, Memory: 40328 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.java
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -1,35 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class Main {
+	static BigInteger count = new BigInteger("0");
+	static StringBuilder sb = new StringBuilder();
 
-    private static StringBuilder sb = new StringBuilder();
-    private static BigInteger[] dp;
+	public static void hanoi(int num, int start, int target, int namoji) {
+		if (num == 1) {
+			sb.append(start).append(" ").append(target).append("\n");
+			return;
+		}
+		hanoi(num - 1, start, namoji, target);
+		hanoi(1, start, target, namoji);
+		hanoi(num - 1, namoji, target, start);
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	public static BigInteger hanoi_num(int n) {
+		BigInteger base = new BigInteger("2");
+		return base.pow(n).subtract(new BigInteger("1"));
+	}
 
-        int n = Integer.parseInt(reader.readLine());
-        dp = new BigInteger[n+1];
-        dp[1] = new BigInteger("1");
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1].multiply(new BigInteger("2")).add(new BigInteger("1"));
-        }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		count = hanoi_num(n);
+		if (n > 20) {
+			System.out.println(count);
+		} else {
+			hanoi(n, 1, 3, 2);
+			System.out.println(count);
+			System.out.println(sb);
+		}
 
-        sb.append(dp[n].toString()).append('\n');
-        if (n <= 20) {
-            recursion(n, 1, 3, 2);
-        }
-        System.out.println(sb);
-    }
+	}
 
-    private static void recursion(int count, int from, int to, int through) throws IOException {
-        if (count == 1) {
-            sb.append(from).append(' ').append(to).append('\n');
-            return;
-        }
-        recursion(count - 1, from, through, to);
-        recursion(1, from, to ,through);
-        recursion(count - 1, through, to, from);
-    }
 }
