f2966f020a16782690a8c9d37a513518c78a3427
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-03 11:58:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 116 ms, Memory: 15088 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,34 +1,44 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer token = new StringTokenizer(reader.readLine());
-        int n = Integer.parseInt(token.nextToken());
-        int m = Integer.parseInt(token.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        token = new StringTokenizer(reader.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(token.nextToken());
-        }
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        int ans = 0;
-        int left = 0;
-        int sum = 0;
-        for (int right = 0; right < n; ++right) {
-            sum += arr[right];
-            while (sum > m && left <= right) {
-                sum -= arr[left];
-                ++left;
-            }
-            if (sum == m) {
-                ++ans;
-            }
-        }
-        System.out.println(ans);
-    }
+		st = new StringTokenizer(br.readLine());
+		int[] nums = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int s = 0;
+		int e = 0;
+		int sum = 0;
+		int cnt = 0;
+
+		while (true) {
+			if (sum >= m) {
+				if (sum == m) {
+					cnt++;
+				}
+				sum -= nums[s++];
+			} else if (e == n) {
+				break;
+			} else {
+				sum += nums[e++];
+			}
+		}
+		
+		System.out.println(cnt);
+
+	}
+
 }
