54197593a8704ce0d749afeaa938565f42825cc7
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-04 14:46:42 +0900

    [Gold V] Title: 신기한 소수, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(buffer.readLine());
+
+        for (int i = 1; i < 10; i++) {
+            dfs(i, 1, n);
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int curr, int depth, int n) {
+        // 현재까지의 수가 소수인지 확인하여 소수 아니면 백트래킹
+        if (!isPrimeNumber(curr)) {
+            return;
+        }
+        if (depth == n) {
+            sb.append(curr).append('\n');
+            return;
+        }
+        for (int i = 1; i < 10; i++) {
+            curr *= 10;
+            curr += i;
+            dfs(curr, depth + 1, n);
+            curr -= i;
+            curr /= 10;
+        }
+    }
+
+    static boolean isPrimeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= (int) Math.floor(Math.sqrt(n)); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
