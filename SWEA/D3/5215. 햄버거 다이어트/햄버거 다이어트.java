080a585f206d388394643d42efd263a0185cd639
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-06 11:16:25 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,408 ms, Memory: 29,504 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,10 +4,6 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-    static int ans;
-    static int[][] ingredients;
-    static int n;
-    static int limit;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -15,13 +11,13 @@ public class Solution {
 
         int t = Integer.parseInt(bf.readLine());
         for (int testCase = 1; testCase <= t; ++testCase) {
-            ans = 0;
+            int ans = 0;
 
             StringTokenizer token = new StringTokenizer(bf.readLine());
-            n = Integer.parseInt(token.nextToken());
-            limit = Integer.parseInt(token.nextToken());
+            int n = Integer.parseInt(token.nextToken());
+            int limit = Integer.parseInt(token.nextToken());
 
-            ingredients = new int[n][2];
+            int[][] ingredients = new int[n][2];
             for (int i = 0; i < n; i++) {
                 token = new StringTokenizer(bf.readLine());
                 int taste = Integer.parseInt(token.nextToken());
@@ -29,27 +25,25 @@ public class Solution {
                 ingredients[i] = new int[] { taste, calorie };
             }
 
-            dfs(0, 0, 0);
+            for (int subset = 0; subset < 0b1 << n; subset++) {
+                int taste = 0;
+                int calorie = 0;
+                for (int i = 0; i < n; i++) {
+                    int mask = 0b1 << i;
+                    if ((subset & mask) > 0) {
+                        taste += ingredients[i][0];
+                        calorie += ingredients[i][1];
+                    }
+                }
+                if (calorie > limit) {
+                    continue;
+                }
+                ans = Integer.max(ans, taste);
+            }
 
             sb.append('#').append(testCase).append(' ')
                     .append(ans).append('\n');
         }
         System.out.println(sb);
-    }
-
-    static void dfs(int taste, int calorie, int depth) {
-        if (calorie > limit) {
-            return;
-        }
-        ans = Integer.max(ans, taste);
-        if (depth == n) {
-            return;
-        }
-        // 그 재료를 쓰는 경우
-        dfs(taste + ingredients[depth][0],
-                calorie + ingredients[depth][1],
-                depth + 1);
-        // 안 쓰는 경우
-        dfs(taste, calorie, depth + 1);
     }
 }
