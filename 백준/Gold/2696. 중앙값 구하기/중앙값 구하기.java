69015fe833b6b27f545811d9074ec7cb4c66d279
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 04:31:28 +0900

    [Gold II] Title: 중앙값 구하기, Time: 372 ms, Memory: 32576 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -13,23 +13,41 @@ public class Main {
             }
 
             StringBuilder sb = new StringBuilder();
-            List<Integer> center = new ArrayList<>();
-            List<Integer> list = new ArrayList<>();
+
+            // 중앙값의 오른쪽 (중앙값보다 큰 값들)
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            // 중앙값의 왼쪽 (중앙값과 같거나 작은 값들)
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
             int count = 0;
+            List<Integer> list = new ArrayList<>();
             for (int i = 0; i < n; i++) {
+                int e = sequence[i];
 
-                list.add(sequence[i]);
+                if (minHeap.size() == maxHeap.size()) {
+                    maxHeap.add(e);
+                } else {
+                    minHeap.add(e);
+                }
 
-                if (i % 2 == 0) {
-                    list.sort(Comparator.naturalOrder());
-                    center.add(list.get(i / 2));
+                // swap
+                if (!minHeap.isEmpty() && minHeap.peek() < maxHeap.peek()) {
+                    int val1 = minHeap.poll();
+                    int val2 = maxHeap.poll();
+                    minHeap.add(val2);
+                    maxHeap.add(val1);
+                }
+
+                // 홀수번째
+                if ((i + 1) % 2 == 1) {
+                    list.add(maxHeap.peek());
                     ++count;
                 }
             }
             sb.append(count).append('\n');
-            for (int i = 0; i < center.size(); i++) {
-                sb.append(center.get(i)).append(' ');
-                if ((i + 1) % 10 == 0 && (i + 1) != center.size()) {
+            for (int i = 0; i < list.size(); i++) {
+                sb.append(list.get(i)).append(' ');
+                if ((i + 1) % 10 == 0 && i + 1 < list.size()) {
                     sb.append('\n');
                 }
             }
