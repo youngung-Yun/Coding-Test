cc723d2c62c5da472044312e006a5b96d301fa87
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 16:08:45 +0900

    [Silver III] Title: 모든 순열, Time: 172 ms, Memory: 19560 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -4,33 +4,74 @@ import java.util.*;
 
 public class Main {
 
+    static int n;
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        dfs(new boolean[n+1], 0, n, new int[n]);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bf.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        while (nextPermutation()) {
+
+        }
+
         System.out.println(sb);
     }
 
+    static boolean nextPermutation() {
+        printArray();
 
-    static void dfs(boolean[] visited, int depth, int n, int[] array) {
-        if (depth == n) {
-            for (int e : array) {
-                sb.append(e).append(' ');
-            }
-            sb.append('\n');
-            return;
+        int idx = n - 2;
+        // 1. 뒤에서부터 오름차순이 깨지는 지점 찾기
+        while (idx >= 0 && arr[idx] > arr[idx + 1]) {
+            --idx;
+        }
+        // 모든 순열 찾음
+        if (idx < 0) {
+            return false;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (visited[i]) {
-                continue;
+        // 2. idx의 뒤에서 arr[idx]보다 크면서 가장 작은 값 찾기
+        int minIdx = 0;
+        for (int i = n - 1; i > idx; i--) {
+            if (arr[idx] < arr[i]) {
+                minIdx = i;
+                break;
             }
-            visited[i] = true;
-            array[depth] = i;
-            dfs(visited, depth + 1, n, array);
-            visited[i] = false;
+        }
+
+        // 3. swap
+        swap(idx, minIdx);
+
+        // 4. 뒷부분 뒤집기
+        reverse(idx + 1);
+
+        return true;
+    }
+
+    static void printArray() {
+        for (int e : arr) {
+            sb.append(e).append(' ');
+        }
+        sb.append('\n');
+    }
+
+    static void swap(int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+    static void reverse(int idx) {
+        int length = n - idx;
+        for (int i = 0; i < length / 2; i++) {
+            swap(idx + i, n - 1 - i);
         }
     }
 }
