fd60c4e63b352e6c3382f3a94022217ca3625f86
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-09 13:19:05 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 185 ms, Memory: 35,072 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,65 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.List;
 
 class Solution {
 
-    static int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    final static int MAX = 100 * 100 * 10;
+    // 상 하 좌 우
+    final static int[][] dirs = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    static int[][] grid;
 
     public static void main(String[] args) throws Exception {
-        final int INF = 100 * 100 * 10 + 1;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        for (int testCase = 1; testCase <= t; testCase++) {
-            int n = Integer.parseInt(br.readLine());
-            int[][] map = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String row = br.readLine();
-                for (int k = 0; k < n; k++) {
-                    map[i][k] = Character.getNumericValue(row.charAt(k));
+        int t = Integer.parseInt(bf.readLine());
+        for (int testCase = 1; testCase <= t; ++testCase) {
+            int n = Integer.parseInt(bf.readLine());
+            grid = new int[n][n];
+            for (int r = 0; r < n; r++) {
+                String row = bf.readLine();
+                for (int c = 0; c < n; c++) {
+                    grid[r][c] = row.charAt(c) - '0';
                 }
             }
 
-            int[][] distance = new int[n][n];
-            for (int[] row : distance) {
-                Arrays.fill(row, INF);
+            int[][] cost = new int[n][n];
+            for (int[] row : cost) {
+                Arrays.fill(row, MAX);
             }
-
-            PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.cost, n2.cost));
-            distance[0][0] = 0;
-            pq.add(new Node(0, 0, 0));
-
+            // [cost, x, y] : 출발 지점에서 (x,y)까지 cost 걸리는 경우
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> Integer.compare(a1[0], a2[0]));
+            pq.add(new int[] {0, 0, 0});
+            cost[0][0] = 0;
             while (!pq.isEmpty()) {
-                Node current = pq.poll();
+                int[] current = pq.poll();
+                int c = current[0];
+                int x = current[1];
+                int y = current[2];
+                if (c > cost[x][y]) {
+                    continue;
+                }
 
                 for (int[] dir : dirs) {
-                    int nx = current.x + dir[0];
-                    int ny = current.y + dir[1];
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
                     if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
                         continue;
                     }
-                    if (distance[current.x][current.y] + current.cost < distance[nx][ny]) {
-                        distance[nx][ny] = distance[current.x][current.y] + current.cost;
-                        pq.add(new Node(nx, ny, map[nx][ny]));
+                    // 출발 지점에서 (x, y)를 거쳐 (nx, ny)로 가는 비용이 현재 (nx, ny)로 가는 비용보다 저렴하면 갱신
+                    if (c + grid[nx][ny] < cost[nx][ny]) {
+                        cost[nx][ny] = c + grid[nx][ny];
+                        pq.add(new int[] {cost[nx][ny], nx, ny});
                     }
                 }
             }
-
-            sb.append('#').append(testCase).append(' ').append(distance[n-1][n-1]).append('\n');
+            sb.append('#').append(testCase).append(' ')
+                    .append(cost[n-1][n-1]).append('\n');
         }
         System.out.println(sb);
-    }
-
-    static class Node {
-        public int x;
-        public int y;
-        public int cost;
-
-        public Node(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
     }
 }
