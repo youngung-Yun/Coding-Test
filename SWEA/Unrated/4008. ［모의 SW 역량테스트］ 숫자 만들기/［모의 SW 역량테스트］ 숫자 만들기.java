76cbff0201ede45af4e2855d279095b8e239a929
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-09 12:05:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 153 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,85 +1,69 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int N, max, min;
-	static int[] opers, nums, result;
-	static boolean[] visited;
+    static int min;
+    static int max;
+    static int[] operators;
+    static int[] operands;
+    static int n;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int testcase = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(bf.readLine());
+        for (int testCase = 1; testCase <= t; testCase++) {
+            n = Integer.parseInt(bf.readLine());
+            min = 100_000_000;
+            max = -100_000_000;
+            // +, -, *, /
+            operators = new int[4];
+            StringTokenizer stk = new StringTokenizer(bf.readLine());
+            for (int i = 0; i < 4; i++) {
+                operators[i] = Integer.parseInt(stk.nextToken());
+            }
 
-		for (int tc = 1; tc <= testcase; tc++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			max = Integer.MIN_VALUE;
-			min = Integer.MAX_VALUE;
-			opers = new int[4];
-			nums = new int[N];
-			result = new int[N - 1];
-			visited = new boolean[N - 1];
+            operands = new int[n];
+            stk = new StringTokenizer(bf.readLine());
+            for (int i = 0; i < n; i++) {
+                int number = Integer.parseInt(stk.nextToken());
+                operands[i] = number;
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 4; i++) {
-				opers[i] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				nums[i] = Integer.parseInt(st.nextToken());
-			}
+            }
 
-			getMaxMin(0, 0, 0);
+            dfs(0, operands[0]);
+            sb.append('#').append(testCase).append(' ')
+                    .append(max - min).append('\n');
+        }
+        System.out.println(sb);
+    }
 
-			System.out.println("#" + tc + " " + (max - min));
+    static void dfs(int depth, int result) {
+        if (depth == n - 1) {
+            min = Integer.min(min, result);
+            max = Integer.max(max, result);
+            return;
+        }
 
-		}
-
-	}
-
-	public static void getMaxMin(int depth, int start, int index) {
-		if (depth == opers[index]) {
-			if (index == 3) {
-				int sum = nums[0];
-				for (int i = 1; i < N; i++) {
-					switch (result[i - 1]) {
-					case 0:
-						sum += nums[i];
-						continue;
-					case 1:
-						sum -= nums[i];
-						continue;
-					case 2:
-						sum *= nums[i];
-						continue;
-					case 3:
-						sum /= nums[i];
-						continue;
-					default:
-						continue;
-					}
-				}
-				max = sum > max ? sum : max;
-				min = sum < min ? sum : min;
-				return;
-			} else {
-				getMaxMin(0, 0, index + 1);
-				return;
-			}
-		}
-
-		for (int i = start; i < N - 1; i++) {
-			if (visited[i])
-				continue;
-			visited[i] = true;
-			result[i] = index;
-			getMaxMin(depth + 1, i + 1, index);
-			visited[i] = false;
-		}
-	}
+        for (int i = 0; i < 4; i++) {
+            if (operators[i] == 0) {
+                continue;
+            }
+            --operators[i];
+            if (i == 0) {
+                dfs(depth + 1, result + operands[depth+1]);
+            } else if (i == 1) {
+                dfs(depth + 1, result - operands[depth+1]);
+            } else if (i == 2) {
+                dfs(depth + 1, result * operands[depth+1]);
+            } else if (i == 3) {
+                dfs(depth + 1, result / operands[depth+1]);
+            }
+            ++operators[i];
+        }
+    }
 }
