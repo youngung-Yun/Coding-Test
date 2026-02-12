610dd8fac4d6f2dff96b967219e1476f927b29a8
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-12 13:14:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 904 ms, Memory: 34,316 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -1,71 +1,51 @@
-import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.List;
+import java.io.IOException;
 
-class Solution {
+public class Solution {
 
-    static final int[][] offsets = {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(bf.readLine());
-        for (int testCase = 1; testCase <= t; ++testCase) {
-            StringTokenizer stk = new StringTokenizer(bf.readLine());
-            int n = Integer.parseInt(stk.nextToken());
-            int m = Integer.parseInt(stk.nextToken());
+		int testcase = Integer.parseInt(st.nextToken());
 
-            int[][] city = new int[n][n];
-            for (int r = 0; r < n; r++) {
-                stk = new StringTokenizer(bf.readLine());
-                for (int c = 0; c < n; c++) {
-                    city[r][c] = Integer.parseInt(stk.nextToken());
-                }
-            }
+		for (int tc = 1; tc <= testcase; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			int[][] map = new int[n][n];
+			int maxHouse = 0;
 
-            int maxHomeCount = 0;
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < n; c++) {
-                    boolean[][] visited = new boolean[n][n];
-                    int homeCount = 0;
-                    for (int k = 0; k < 2 * n; k++) {
-                        for (int dx = 0; dx <= k; dx++) {
-                            int dy = k - dx;
-                            for (int[] offset : offsets) {
-                                int nx = r + (dx * offset[0]);
-                                int ny = c + (dy * offset[1]);
-                                if (!isValid(nx, ny, n)) {
-                                    continue;
-                                }
-                                if (visited[nx][ny]) {
-                                    continue;
-                                }
-                                if (city[nx][ny] == 1) {
-                                    ++homeCount;
-                                }
-                                visited[nx][ny] = true;
-                            }
-                        }
-                        int cost = (k * k) + (k + 1) * (k + 1);
-                        int benefit = (homeCount * m) - cost;
-                        if (benefit >= 0) {
-                            maxHomeCount = Integer.max(maxHomeCount, homeCount);
-                        }
-                    }
-                }
-            }
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < (2 * n - 1); k++) {
+						int currentHouse = 0;
+						for (int r = 0; r < n; r++) {
+							for (int c = 0; c < n; c++) {
+								if (Math.abs(i - r) + Math.abs(j - c) <= k) {
+									if (map[r][c] == 1)
+										currentHouse++;
+								}
+							}
+						}
+						if (currentHouse * m >= Math.pow(k + 1, 2) + Math.pow(k, 2)) {
+							maxHouse = currentHouse > maxHouse ? currentHouse : maxHouse;
+						}
+					}
+				}
+			}
 
-            sb.append('#').append(testCase).append(' ')
-                    .append(maxHomeCount).append('\n');
-        }
-        System.out.println(sb);
-    }
+			System.out.println("#" + tc + " " + maxHouse);
 
-    private static boolean isValid(int x, int y, int n) {
-        return x >= 0 && y >= 0 && x < n && y < n;
-    }
+		}
+	}
 }
