4907a2e0a1b6c2fdae376d2040d0f146aa173328
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-13 13:46:04 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 130 ms, Memory: 31,912 KB -BaekjoonHub

--- SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -5,62 +5,24 @@ import java.util.List;
 
 public class Solution {
 
-    static long MAX = 1_000_000_000_000L;
-    static List<Long> powers = new ArrayList<>();
-
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        powers = getPowers();
 
         int t = Integer.parseInt(bf.readLine());
         for (int testcase = 1; testcase <= t; testcase++) {
             long n = Long.parseLong(bf.readLine());
 
-            long ans = computeMinCount(n);
-
+            int ans = 0;
+            while (n > 2) {
+                long power = (long) Math.pow(Math.ceil(Math.sqrt(n)), 2);
+                ans += (power - n);
+                n = (long) Math.sqrt(power);
+                ++ans;
+            }
             sb.append('#').append(testcase).append(' ')
                     .append(ans).append('\n');
         }
         System.out.println(sb);
-    }
-
-    static long computeMinCount(long n) {
-        long count = 0L;
-        while (n > 2) {
-            long power = binarySearch(n);
-            count += (power - n);
-            n = (long) Math.sqrt(power);
-            ++count;
-        }
-        return count;
-    }
-
-    // 자신보다 크거나 같은 멱수 중 가장 작은 수를 구해야 함
-    // lower case
-    static long binarySearch(long n) {
-        int low = 0;
-        int high = powers.size();
-
-        int ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (powers.get(mid) < n) {
-                low = mid + 1;
-            } else {
-                ans = mid;
-                high = mid - 1;
-            }
-        }
-        return powers.get(ans);
-    }
-
-    static List<Long> getPowers() {
-        List<Long> list = new ArrayList<>();
-        for (Long n = 2L; n * n <= MAX; n++) {
-            list.add(n * n);
-        }
-        return list;
     }
 }
