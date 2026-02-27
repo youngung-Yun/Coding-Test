db7484b78c7231973f4db114f192f01ccda82a72
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-27 09:55:23 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 104 ms, Memory: 14200 KB, Score: 39 point -BaekjoonHub

--- 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
@@ -5,31 +5,57 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int[] dp;
+    static int[] food;
+    static int n;
+    static int k;
+    static int ans = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer stk = new StringTokenizer(bf.readLine());
 
-        int n = Integer.parseInt(stk.nextToken());
-        int k = Integer.parseInt(stk.nextToken());
+        n = Integer.parseInt(stk.nextToken());
+        k = Integer.parseInt(stk.nextToken());
 
-        int[] food = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        int ans = 0;
-        for (int binary = 0; binary < (0b1 << n); binary++) {
-            int pleasure = 0;
-            int energy = 0;
-            for (int seq = 0; seq < n; seq++) {
-                if ((binary & (0b1 << seq)) != 0) {
-                    pleasure += food[seq];
-                    if (pleasure >= k) {
-                        energy += pleasure - k;
-                        pleasure = 0;
-                    }
-                }
-            }
-            ans = Integer.max(ans, energy);
+        food = new int[n];
+        stk = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            food[i] = Integer.parseInt(stk.nextToken());
         }
+
+        // i번째 먹이를 먹기 시작했을 때 만족도를 채우게 되는 인덱스
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        for (int start = 0; start < n; start++) {
+            int curr = start;
+            int pleasure = 0;
+            while (pleasure < k && curr < n) {
+                pleasure += food[curr++];
+            }
+            dp[start] = --curr;
+        }
+
+        recursion(0, 0);
+
         System.out.println(ans);
+    }
+
+    private static void recursion(int start, int totalPleasure) {
+        if (start == n) {
+            ans = Integer.max(ans, totalPleasure);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            int end = dp[i];
+            int pleasure = 0;
+            for (int j = i; j <= end; j++) {
+                pleasure += food[j];
+            }
+            recursion(end + 1, totalPleasure + Integer.max(pleasure - k, 0));
+        }
     }
 }
