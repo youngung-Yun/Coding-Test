7bb3a1ad3798babfbc193ee027dfb2e1780bc420
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-07 22:21:41 +0900

    [Gold V] Title: 괄호의 값, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 public class Main {
 
@@ -13,40 +15,41 @@ public class Main {
         boolean isCorrect = true;
 
         Deque<Character> bracketStack = new ArrayDeque<>();
-        Deque<Integer> numberStack = new ArrayDeque<>();
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>());
         for (char bracket : sequence.toCharArray()) {
             if (bracket == '(' || bracket == '[') {
                 bracketStack.push(bracket);
-                numberStack.push(-1);
+                list.add(new ArrayList<>());
             } else {
                 char open = bracket == ')' ? '(' : '[';
                 if (bracketStack.isEmpty() || bracketStack.peek() != open) {
                     isCorrect = false;
                     break;
                 }
+                // 여는 괄호 제거
                 bracketStack.pop();
-                int sum = 0;
-                boolean hasInBracket = false;
-                while (numberStack.peek() != -1) {
-                    hasInBracket = true;
-                    sum += numberStack.pop();
-                }
-                numberStack.pop();
-                if (!hasInBracket) {
+                int sum;
+                if (list.get(list.size() - 1).isEmpty()) {
                     sum = 1;
-                }
-
-                if (bracket == ')') {
-                    numberStack.push(sum * 2);
                 } else {
-                    numberStack.push(sum * 3);
+                    sum = 0;
+                    for (int number : list.get(list.size() - 1)) {
+                        sum += number;
+                    }
+                }
+                list.remove(list.size() - 1);
+                if (bracket == ')') {
+                    list.get(list.size() - 1).add(sum * 2);
+                } else {
+                    list.get(list.size() - 1).add(sum * 3);
                 }
             }
         }
 
         int ans = 0;
-        while (!numberStack.isEmpty()) {
-            ans += numberStack.pop();
+        for (int number : list.get(0)) {
+            ans += number;
         }
 
         if (isCorrect && bracketStack.isEmpty()) {
