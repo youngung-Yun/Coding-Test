67d322529a43fe755723b08cf7355047508ad111
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-12 11:27:32 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 232 ms, Memory: 21032 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,66 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    final static int MAX = 125 * 125 * 100;
-    final static int[] dx = {0, 0, 1, -1};
-    final static int[] dy = {1, -1, 0, 0};
+    final static int[][] dirs = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+    final static int INF = 125 * 125 * 10;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int testCase = 1;
+        StringBuilder sb = new StringBuilder();
+        int tc = 0;
         while (true) {
+            ++tc;
             int n = Integer.parseInt(bf.readLine());
             if (n == 0) {
                 break;
             }
+
             int[][] cave = new int[n][n];
             for (int r = 0; r < n; r++) {
-                StringTokenizer token = new StringTokenizer(bf.readLine());
+                StringTokenizer stk = new StringTokenizer(bf.readLine());
                 for (int c = 0; c < n; c++) {
-                    cave[r][c] = Integer.parseInt(token.nextToken());
+                    cave[r][c] = Integer.parseInt(stk.nextToken());
                 }
             }
 
-            int[][] cost = new int[n][n];
-            for (int[] row : cost) {
-                Arrays.fill(row, MAX);
+            int[][] distance = new int[n][n];
+            for (int[] row : distance) {
+                Arrays.fill(row, INF);
             }
-            // [cost, destX, destY]
-            PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> Integer.compare(a1[0], a2[0]));
-            pq.add(new int[] {cave[0][0], 0, 0});
-            while (!pq.isEmpty()) {
-                int[] current = pq.poll();
-                if (current[0] > cost[current[1]][current[2]]) {
-                    continue;
-                }
 
-                for (int i = 0; i < 4; i++) {
-                    int nx = current[1] + dx[i];
-                    int ny = current[2] + dy[i];
-                    if (!isValid(nx, ny, n)) {
-                        continue;
-                    }
-                    // 현재 구한 (nx, ny)로 가는 비용보다 (x, y)까지 가는 경로
-                    // + (x, y)에서 (nx, ny) 가는 경로가 더 싸면 갱신
-                    if (cost[nx][ny] > current[0] + cave[nx][ny]) {
-                        cost[nx][ny] = current[0] + cave[nx][ny];
-                        pq.add(new int[] {cost[nx][ny], nx, ny});
-                    }
-                }
-            }
-            int ans = cost[n-1][n-1];
-            sb.append("Problem ").append(testCase).append(": ").append(ans).append('\n');
-            ++testCase;
+            findShortestPath(cave, distance, n);
+            sb.append("Problem ").append(tc).append(": ")
+                    .append(distance[n-1][n-1]).append('\n');
         }
         System.out.println(sb);
     }
 
-    static boolean isValid(int x, int y, int n) {
-        return x >= 0 && y >= 0 && x < n && y < n;
+    private static void findShortestPath(int[][] weight, int[][] distance, int n) {
+        // [r, c, w]
+        distance[0][0] = weight[0][0];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e[2]));
+        pq.add(new int[] {0, 0, weight[0][0]});
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int r = now[0];
+            int c = now[1];
+            int w = now[2];
+
+            if (distance[r][c] < w) {
+                continue;
+            }
+
+            for (int[] dir : dirs) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+                if (!isValid(nr, nc, n)) {
+                    continue;
+                }
+                if (distance[nr][nc] > distance[r][c] + weight[nr][nc]) {
+                    distance[nr][nc] = distance[r][c] + weight[nr][nc];
+                    pq.add(new int[] {nr, nc, distance[nr][nc]});
+                }
+            }
+        }
     }
 
+    private static boolean isValid(int r, int c, int n) {
+        return r >= 0 && c >= 0 && r < n && c < n;
+    }
 }
