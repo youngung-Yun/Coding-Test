ef7fc990b7a52874330c88eae7d32987a0168883
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-14 02:02:43 +0900

    [Gold II] Title: Fibonacci Sequence, Time: 112 ms, Memory: 15140 KB -BaekjoonHub

---
+++ 백준/Gold/13075. Fibonacci Sequence/Fibonacci Sequence.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    final static long[][] zeroMatrix = {
+            {0L, 0L},
+            {0L, 0L}
+    };
+    final static long[][] baseMatrix = {
+            {1L, 1L},
+            {1L, 0L}
+    };
+    final static int MOD = 1_000_000_000;
+    final static int W = 2;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(bf.readLine());
+        while (tc-- > 0) {
+            long exp = Long.parseLong(bf.readLine());
+            if (exp == -1) {
+                break;
+            }
+            System.out.println(recursion(exp)[0][1] % MOD);
+        }
+    }
+
+    private static long[][] recursion(long exp) {
+        if (exp == 0L) {
+            return zeroMatrix;
+        } else if (exp == 1L) {
+            return baseMatrix;
+        }
+
+        long halfExp = exp / 2L;
+        long[][] halfPower = recursion(halfExp);
+        long[][] result = multiplyMatrix(halfPower, halfPower);
+        if (exp % 2 == 0) {
+            return result;
+        } else {
+            return multiplyMatrix(result, baseMatrix);
+        }
+    }
+
+    private static long[][] multiplyMatrix(long[][] a, long[][] b) {
+        long[][] result = new long[W][W];
+        for (int r = 0; r < W; r++) {
+            for (int c = 0; c < W; c++) {
+                long sum = 0;
+                for (int i = 0; i < W; i++) {
+                    sum += (a[r][i] * b[i][c]);
+                }
+                result[r][c] = sum % MOD;
+            }
+        }
+        return result;
+    }
+}
