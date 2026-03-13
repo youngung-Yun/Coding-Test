87d1e4b609352c130eda902639f347fcd56751f7
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-13 12:01:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 184 ms, Memory: 23868 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 
-    final static int INF = -1;
+    final static int INF = 100_001;
     final static int MAX = 200_000;
 
     public static void main(String[] args) throws Exception {
@@ -16,6 +16,7 @@ public class Main {
 
         int[] distance = new int[MAX+1];
         Arrays.fill(distance, INF);
+        distance[chaser] = 0;
         Deque<int[]> deque = new ArrayDeque<>();
         deque.offerLast(new int[] {chaser, 0});
 
@@ -24,31 +25,23 @@ public class Main {
             int coord = now[0];
             int d = now[1];
 
-            if (distance[coord] != INF) {
-                continue;
-            }
-            // 큐에서 꺼낼 때 최단경로 확정
-            distance[coord] = d;
-            if (coord == target) {
-                break;
-            }
-
             if (coord > 0) {
                 for (int next = coord * 2; next <= MAX; next *= 2) {
-                    if (distance[next] == INF) {
+                    if (distance[next] > d) {
                         deque.offerFirst(new int[] {next, d});
+                        distance[next] = d;
                     }
                 }
             }
-
-            if (coord > 0 && distance[coord-1] == INF) {
+            if (coord > 0 && distance[coord-1] > d) {
                 deque.offerLast(new int[] {coord - 1, d + 1});
+                distance[coord-1] = d + 1;
             }
-            if (coord < MAX && distance[coord+1] == INF) {
+            if (coord < MAX && distance[coord+1] > d) {
                 deque.offerLast(new int[] {coord + 1, d + 1});
+                distance[coord+1] = d + 1;
             }
         }
-
         System.out.println(distance[target]);
     }
 }
