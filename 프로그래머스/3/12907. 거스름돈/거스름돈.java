603355f3ed59c9ce3da21fa422b8e65afed9a95b
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-16 13:06:13 +0900

    [level 3] Title: 거스름돈, Time: 12.88 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    
+    final static int MOD = 1_000_000_007;
+
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for (int currency : money) {
+            for (int m = currency; m <= n; m++) {
+                dp[m] =  (dp[m] + dp[m-currency]) % MOD;;
+            }
+        }
+        
+        return dp[n];
+    }
+}
