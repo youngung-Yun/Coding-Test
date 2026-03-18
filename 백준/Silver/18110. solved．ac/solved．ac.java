8419b60e185ef304979392fa1bfa2e3fb0dc0083
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-18 11:26:36 +0900

    [Silver IV] Title: solved.ac, Time: 472 ms, Memory: 42456 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,59 +1,35 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        int[] array = new int[n];
-        int[] counts = new int[31];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		for(int i=0; i<n;i++) {
+			int num = Integer.parseInt(br.readLine());
+			list.add(num);
+		}
+		list.sort((a,b)->a-b);
+		int firstIndex=0;
+		int lastIndex=n-1;
+		
+		double del = n*0.3;
+		del = Math.round(del/2);
+		
+		firstIndex += del;
+		lastIndex -=del;
+		
+		double total=0;
+		for(int i=firstIndex; i<=lastIndex;i++) {
+			total+= list.get(i);
+		}
+		
+		total= Math.round(total/(n-(del*2)));
+		
+		System.out.println((int)total);
+		
+	}
+	
 
-        for (int i = 0; i < n; i++) {
-            int difficulty = Integer.parseInt(br.readLine());
-            ++counts[difficulty];
-        }
-
-        int trimmedMean = (int) Math.round(n * 0.15);
-        int left = trimmedMean;
-        int right = trimmedMean;
-        int leftIndex = 1;
-        int rightIndex = 30;
-        
-        while (left > 0) {
-            if (counts[leftIndex] >= left) {
-                counts[leftIndex] -= left;
-                left = 0;
-            }
-            else {
-                left -= counts[leftIndex];
-                counts[leftIndex] = 0;
-                ++leftIndex;
-            }
-        }
-
-        while (right > 0) {
-            if (counts[rightIndex] >= right) {
-                counts[rightIndex] -= right;
-                right = 0;
-            }
-            else {
-                right -= counts[rightIndex];
-                counts[rightIndex] = 0;
-                --rightIndex;
-            }
-        }
-
-        int total = 0;
-        for (int i = 1; i < counts.length; i++) {
-            total += i * counts[i];
-        }
-
-        long answer = Math.round(total * 1.0 / (n - 2 * trimmedMean));
-
-        bw.write(String.valueOf(answer));
-        bw.flush();
-    }
 }
