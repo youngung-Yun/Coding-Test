86417c14ebea9f4b5f0136de76905fd46accbc38
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-22 10:56:28 +0900

    [Gold II] Title: 보석 도둑, Time: 1384 ms, Memory: 128780 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+
+        // 무게 오름차순
+        PriorityQueue<int[]> jewels = new PriorityQueue<>((a1, a2) -> Integer.compare(a1[0], a2[0]));
+        // 가격 내림차순
+        PriorityQueue<int[]> mostValuable = new PriorityQueue<>((a1, a2) -> Integer.compare(a2[1], a1[1]));
+
+        for (int i = 0; i < n; i++) {
+            stk = new StringTokenizer(bf.readLine());
+            int mass = Integer.parseInt(stk.nextToken());
+            int value = Integer.parseInt(stk.nextToken());
+            jewels.add(new int[] {mass, value});
+        }
+
+        PriorityQueue<Integer> bags = new PriorityQueue<>(Comparator.naturalOrder());
+        for (int i = 0; i < k; i++) {
+            bags.add(Integer.parseInt(bf.readLine()));
+        }
+
+        long ans = 0L;
+        while (!bags.isEmpty()) {
+            int bag = bags.poll();
+            while (!jewels.isEmpty() && jewels.peek()[0] <= bag) {
+                mostValuable.add(jewels.poll());
+            }
+            if (mostValuable.isEmpty()) {
+                continue;
+            }
+            ans += mostValuable.poll()[1];
+        }
+
+        System.out.println(ans);
+    }
+
+}
