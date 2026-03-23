080ccb3d5b3b330fdef6745602c12820211fe65c
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-23 12:17:55 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 196 ms, Memory: 30,656 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,128 +1,93 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
+public class Solution {
+	static int n;
+	static int[][] map;
+	static List<int[]> core;
+	static int[] dx= {0,0,-1,1};
+	static int[] dy= {1,-1,0,0};
+	static int[] min;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+	
+		for(int test=1;test<=t;test++) {
+			n = Integer.parseInt(br.readLine().trim());
+			core = new ArrayList<>();
+			map = new int[n][n];
+			min = new int[2];
+			min[0] = Integer.MAX_VALUE;
+			for(int i=0; i<n;i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j=0; j<n;j++) {
+					map[i][j]= Integer.parseInt(st.nextToken());
+					if((j!=0&&i!=0&&j!=n-1&&i!=n-1)&&(map[i][j]==1)) {
+						core.add(new int[] {j,i});
+					}
+				}
+			}
+			check(0,0,0);
+			System.out.println("#"+test+" "+min[0]);
+		}
 
-class Solution {
+	}
+	static void check(int cur,int cnt ,int connedted) {
+		if(cur==core.size()) {
+			if(min[1]<connedted) {
+				min[0] = cnt;
+				min[1] = connedted;
+			}else if(min[1]==connedted) {
+				min[0] = Math.min(cnt, min[0]);
+			}
+			return;
+		}
+		
+		check(cur+1, cnt,connedted);
+		int[] curCore= core.get(cur);
+		int x = curCore[0];
+		int y = curCore[1];
+		
+		for(int i=0; i<4;i++) {
+			if(canCn(x,y,i)) {
+				int count = connect(x,y,i);
+				check(cur+1,cnt+count,connedted+1);
+				disConnect(x,y,i);
+			}
+		}
+		
+	}
+	static boolean canCn(int x, int y, int dir) {
+		while(x!=0&&y!=0&&x!=n-1&&y!=n-1) {
+			x+=dx[dir];
+			y+=dy[dir];
+			if(map[y][x]==1) {
+				return false;
+			}else if(map[y][x]==2) {
+				return false;
+			}
+		}
+		return true;
+	}
+	static int connect(int x, int y, int dir) {
+		int count=0;
+		while(x!=0&&y!=0&&x!=n-1&&y!=n-1) {
+			x+=dx[dir];
+			y+=dy[dir];
+			map[y][x]=2;
+			count++;
+			}
+		return count;
+		}
+	static void disConnect(int x, int y, int dir) {
+		while(x!=0&&y!=0&&x!=n-1&&y!=n-1) {
+			x+=dx[dir];
+			y+=dy[dir];
+			if(map[y][x]==2)map[y][x]=0;
+			}
+		}
+	static boolean isValid(int x, int y) {
+		return !(x<0||y<0||x>=n||y>=n);
+	}
+		
+	}
 
-    static int maxConnectedCellCount;
-    static int minFiberCount;
-    static int n;
-    static int[][] processor;
-    static List<int[]> cellList;
-
-    // 0, 상, 하, 좌, 우
-    static int[][] dirs = { {}, {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(bf.readLine());
-        for (int testCase = 1; testCase <= t; ++testCase) {
-            maxConnectedCellCount = 0;
-            minFiberCount = 0;
-
-            n = Integer.parseInt(bf.readLine());
-            processor = new int[n][n];
-            cellList = new ArrayList<>();
-            for (int r = 0; r < n; r++) {
-                StringTokenizer token = new StringTokenizer(bf.readLine());
-                for (int c = 0; c < n; c++) {
-                    int unit = Integer.parseInt(token.nextToken());
-                    processor[r][c] = unit;
-                    if (unit == 1) {
-                        cellList.add(new int[] {r, c});
-                    }
-                }
-            }
-            dfs(0, cellList.size(), 0, 0);
-            sb.append('#').append(testCase).append(' ')
-                    .append(minFiberCount).append('\n');
-        }
-        System.out.println(sb);
-    }
-
-    static void dfs(int depth, int n, int connectCell, int fiber) {
-        if (depth == n) {
-            if (maxConnectedCellCount < connectCell) {
-                maxConnectedCellCount = connectCell;
-                minFiberCount = fiber;
-            } else if (maxConnectedCellCount == connectCell) {
-                minFiberCount = Integer.min(minFiberCount, fiber);
-            }
-            return;
-        }
-
-        // 전선 = -1
-        for (int i = 0; i < 5; i++) {
-            // 해당 셀 연결하지 않음
-            if (i == 0) {
-                dfs(depth + 1, n, connectCell, fiber);
-                continue;
-            }
-            int[] cell = cellList.get(depth);
-            int x = cell[0];
-            int y = cell[1];
-            int[] dir = dirs[i];
-            if (canAddFiber(x, y, dir)) {
-                int addedfiber = addFiber(x, y, dir);
-                ++connectCell;
-                dfs(depth + 1, n, connectCell, fiber + addedfiber);
-                removeFiber(x, y, dir);
-                --connectCell;
-            }
-        }
-    }
-
-    static boolean canAddFiber(int x, int y, int[] dir) {
-        int nx = x + dir[0];
-        int ny = y + dir[1];
-        while (isValid(nx, ny)) {
-            // 전선 겹치거나 셀이 있음
-            if (processor[nx][ny] == -1 || processor[nx][ny] == 1) {
-                return false;
-            }
-            nx += dir[0];
-            ny += dir[1];
-        }
-        return true;
-    }
-
-    static int addFiber(int x, int y, int[] dir) {
-        int fiberCount = 0;
-        int nx = x + dir[0];
-        int ny = y + dir[1];
-        while (isValid(nx, ny)) {
-            processor[nx][ny] = -1;
-            ++fiberCount;
-            nx += dir[0];
-            ny += dir[1];
-        }
-        return fiberCount;
-    }
-
-    static void removeFiber(int x, int y, int[] dir) {
-        int nx = x + dir[0];
-        int ny = y + dir[1];
-        while (isValid(nx, ny)) {
-            processor[nx][ny] = 0;
-            nx += dir[0];
-            ny += dir[1];
-        }
-    }
-
-    static boolean isValid(int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < n;
-    }
-
-    static void printProcessor() {
-        StringBuilder sb = new StringBuilder();
-        for (int[] row : processor) {
-            for (int col : row) {
-                sb.append(col).append(' ');
-            }
-            sb.append('\n');
-        }
-        sb.append('\n');
-        System.out.println(sb);
-    }
-}
