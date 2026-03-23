4fd71ba067ea710c5b92d92fad97869fe52bcd0e
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-03-23 09:54:54 +0900

    [Gold IV] Title: 오큰수, Time: 716 ms, Memory: 207792 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -3,27 +3,26 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bf.readLine());
-        int[] arr = new int[n];
-        StringTokenizer stk = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(stk.nextToken());
-        }
 
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        // [idx, number]
+        Deque<int[]> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < n; i++) {
-            int curr = arr[i];
-            while (!stack.isEmpty() && arr[stack.peek()] < curr) {
-                ans[stack.pop()] = curr;
+        StringTokenizer stk = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < n; i++) {
+            int number = Integer.parseInt(stk.nextToken());
+            while (!stack.isEmpty() && stack.peek()[1] < number) {
+                int[] lower = stack.pop();
+                ans[lower[0]] = number;
             }
-            stack.push(i);
+            stack.push(new int[] {i, number});
         }
 
         StringBuilder sb = new StringBuilder();
